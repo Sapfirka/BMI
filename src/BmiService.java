8cc6b93df6589444e49139189a206e5bd9c6aec3
Sapfirka <sapfirka@mail.ru>
2023-01-28 18:26:32 +0300

    first commit

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+    public double facts(double weight, double hight) {
+        double result;
+         result = weight / (hight * hight);
+
+        return result;
+    }
+}
