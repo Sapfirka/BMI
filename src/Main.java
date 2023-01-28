7e3c715917282f66923615b736289edbf23f13e1
Sapfirka <122283094+Sapfirka@users.noreply.github.com>
2023-01-28 18:48:24 +0300

    Update Main.java
    Reformate.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double index = service.facts(50,1.6);
+        double index = service.facts(50, 1.6);
         System.out.println("Индекс массы тела:");
         System.out.println();
         System.out.println(index);
